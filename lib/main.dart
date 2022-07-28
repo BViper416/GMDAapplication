93f604452f4cc7194c339f175d8312daf96c3745
Manish Kumar Singh <manish@Manishs-MacBook-Pro.local>
2022-07-28 21:54:46 +0530

    thursday code push and pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gmda/pages/home_page.dart';
 import 'package:gmda/pages/login_page.dart';
+import 'package:gmda/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(Myapp());
@@ -13,12 +15,13 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light,
         theme: ThemeData(primarySwatch: Colors.green),
         routes: {
           "/": (context) => LoginPage(),
-          "/Homepage": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
+          MyRoutes.homeroute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
         });
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,56 +1,114 @@
 import "package:flutter/material.dart";
+import 'package:flutter_svg/svg.dart';
+import 'package:gmda/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset("assets/image/logo.png"),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical:2,horizontal:15),
-            child: Text("Let's start by",
-                style: TextStyle(fontSize: 20)),
+      color: Colors.white,
+      child: Column(
+        children: [
+          SvgPicture.asset(
+            "assets/image/Logo.svg",
+            fit: BoxFit.cover,
           ),
-        ),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical:2,horizontal:15),
-            child: Text("Logging in your account",
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(children: [
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Username/Email-Id", labelText: "Username"),
+
+          Padding(
+            padding: EdgeInsets.all(0.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "Welcome $name",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
             ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "Enter Password", labelText: "Password"),
-            )
-          ]),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-            onPressed: () {
-              print("Welcome User");
+          ),
+
+          //ignore: unnecessary_new
+
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 15),
+              child: Text("Let's start by", style: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+              child: Text("Logging in your account",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(children: [
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Username/Email-Id", labelText: "Username"),
+                  onChanged: (value) {
+                    name = value;
+
+                    setState(() {});
+                  }),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              )
+            ]),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: () async {
+              setState(() {
+                changeButton = true;
+              });
+              await Future.delayed(Duration(seconds: 1));
+              Navigator.pushNamed(context, MyRoutes.homeroute);
             },
-            child: Text(
-              "SIGN IN",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ))
-      ],
-    ));
+            child: AnimatedContainer(
+              duration: Duration(seconds: 5),
+              width:150,
+              height: 50,
+              //color: Colors.green,
+              alignment: Alignment.center,
+              child: changeButton ? Icon(Icons.done,color: Colors.white,)
+               : Text(
+                "SIGN IN",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(8)),
+            ),
+          )
+
+          // ElevatedButton(
+          //     onPressed: () {
+          //       Navigator.pushNamed(context, MyRoutes.homeroute);
+          //     },
+          //     child: Text("SIGN IN",
+          //         style: TextStyle(fontWeight: FontWeight.bold)))
+        ],
+      ),
+    );
   }
 }
